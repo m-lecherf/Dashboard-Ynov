ce3acbe224abe5e179d7b02a43852aee14c1ebe7
m-lecherf <mathis.lecherfpro@gmail.com>
2025-01-23 14:38:10 +0100

    Update Processing.py

--- pages/Processing.py
+++ pages/Processing.py
@@ -6,6 +6,4 @@ if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file)
     edited_df = st.data_editor(dataframe)
-    if st.button('Save'):
-        st.write(edited_df)
     st.download_button('Download', edited_df.to_csv(), file_name='data.csv', mime='text/csv')
